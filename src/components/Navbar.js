2eb0d6ecbcf74e3351e3d4d3fbb6e80024bfb9fd
Asa DeWeese <deweesa@Asas-MacBook-Air.local>
2024-04-15 17:08:22 -0700

    Adding navbar class

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+  return(
+    <nav className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px8">
+        <div className="realtive flex h-16 items-center justify-between">
+          
+        </div>
+      </div>
+    </nav>
+  ) 
+}
+
+export default Navbar;
