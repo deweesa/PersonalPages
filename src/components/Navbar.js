a29cc9c7a701912ac92969d0cfafd9a1be145b3b
deweesa <asadeweese@gmail.com>
2024-07-23 20:24:35 -0700

    Adding router to project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,9 @@
-// the grey color is just rending as black nee dot check if that's a real color (I bet it isn't)
 function Navbar() {
   return(
    <nav className="nav hidden flex flex-col lg:block">
     <ul className="mt-16">
-      <li><a href="/#about">About</a></li>
-      <li><a href="/#experience">Experience</a></li>
+      <li><a className="nav-text group" href="/#about"><span className="group-hover:text-red-800 group-focus-visible:text-red-800">About</span></a></li>
+      <li><a className="hover:text-blue-800 focus:text-blue-800" href="/#experience">Experience</a></li>
       <li><a href="/#projects">Projects</a></li>
     </ul>
    </nav> 
