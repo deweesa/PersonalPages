b3871016218705c750e79e89699dbfdbc91c59e1
deweesa <asadeweese@gmail.com>
2024-07-27 21:39:17 -0700

    Have migrated to nextjs

---
+++ src/app/components/Projects.js
@@ -0,0 +1,18 @@
+export default function Projects() {
+  return(
+    <div id="projects" className="">
+      <ul>
+        <li>These are my projects</li>
+        <li>These are my projects</li>
+        <li>These are my projects</li>
+        <li>These are my projects</li>
+        <li>These are my projects</li>
+        <li>These are my projects</li>
+        <li>These are my projects</li>
+        <li>These are my projects</li>
+        <li>These are my projects</li>
+        <li>These are my projects</li>
+      </ul>
+    </div> 
+  )
+}
